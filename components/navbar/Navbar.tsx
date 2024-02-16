dfc03159d09ce3dc5a49b0518c1b5353ab5e37f4
Sma-source <rabhi.smain@gmail.com>
2024-02-16 17:26:30 +0100

    add style to Navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="w-full justify-between items-center flex p-5 xl:p-0">
+      Navbar
+    </nav>
+  );
 };
 
 export default Navbar;
