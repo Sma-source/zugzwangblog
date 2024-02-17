69c0ead07ddb259c90464314482a1867af033e97
Sma-source <rabhi.smain@gmail.com>
2024-02-17 16:09:12 +0100

    modify navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
 
 const Navbar = () => {
   return (
     <nav className="w-full justify-between items-center flex p-5 xl:p-0">
-      Navbar
+      <Link className="font-bold text-2xl" href="/">
+        Zugzwang Blog
+      </Link>
+      <Button className="flex items-center gap-2" variant="outline">
+        Login
+      </Button>
     </nav>
   );
 };
