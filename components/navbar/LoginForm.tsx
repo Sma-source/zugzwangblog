d2e6371ad9dc6f050f4076b2326cd8364dee2a82
Sma-source <rabhi.smain@gmail.com>
2024-02-17 16:39:21 +0100

    add handlelogin function

--- components/navbar/LoginForm.tsx
+++ components/navbar/LoginForm.tsx
@@ -1,7 +1,21 @@
+"use client";
+import { createBrowserClient } from "@supabase/ssr";
 import React from "react";
 import { Button } from "../ui/button";
 
 const LoginForm = () => {
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+  const handleLogin = () => {
+    supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: location.origin + "/auth/callback",
+      },
+    });
+  };
   return (
     <Button className="flex items-center gap-2" variant="outline">
       Login
