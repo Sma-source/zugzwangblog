94e4bae13e4300cab7448f613127c37d5d121f2c
Sma-source <rabhi.smain@gmail.com>
2024-02-18 17:06:47 +0100

    check if user connected or not

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,14 +1,17 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import LoginForm from "./LoginForm";
+import { useUser } from "@/lib/store/user";
 
 const Navbar = () => {
+  const user = useUser((state) => state.user);
   return (
     <nav className="w-full justify-between items-center flex p-5 xl:p-0">
       <Link className="font-bold text-2xl" href="/">
         Zugzwang Blog
       </Link>
-      <LoginForm />
+      {user ? <h1>profile</h1> : <LoginForm />}
     </nav>
   );
 };
