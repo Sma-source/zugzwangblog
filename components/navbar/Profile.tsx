5fa28f41f5743915281668c93bc75b21de2ec656
Sma-source <rabhi.smain@gmail.com>
2024-02-19 17:38:10 +0100

    install popover shadcdn and use it

--- components/navbar/Profile.tsx
+++ components/navbar/Profile.tsx
@@ -1,17 +1,27 @@
 import { useUser } from "@/lib/store/user";
 import Image from "next/image";
 import React from "react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 const Profile = () => {
   const user = useUser((state) => state.user);
   return (
-    <Image
-      src={user?.user_metadata.avatar_url}
-      alt="profile img"
-      width={50}
-      height={50}
-      className="rounded-full ring-2 ring-violet-500"
-    />
+    <Popover>
+      <PopoverTrigger>
+        <Image
+          src={user?.user_metadata.avatar_url}
+          alt="profile img"
+          width={50}
+          height={50}
+          className="rounded-full ring-2 ring-violet-500"
+        />
+      </PopoverTrigger>
+      <PopoverContent>Place content for the popover here.</PopoverContent>
+    </Popover>
   );
 };
 
