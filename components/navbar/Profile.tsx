a617265df6da4fae59240fa5d2add8fb5aa14d98
Sma-source <rabhi.smain@gmail.com>
2024-02-19 17:57:22 +0100

    add button profile

--- components/navbar/Profile.tsx
+++ components/navbar/Profile.tsx
@@ -6,6 +6,8 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 const Profile = () => {
   const user = useUser((state) => state.user);
@@ -20,7 +22,27 @@ const Profile = () => {
           className="rounded-full ring-2 ring-violet-500"
         />
       </PopoverTrigger>
-      <PopoverContent>Place content for the popover here.</PopoverContent>
+      <PopoverContent className="space-y-3 divide-y p-2" side="bottom">
+        <div className="px-4">
+          <p className="text-sm">{user?.user_metadata.user_name}</p>
+          <p className="text-sm text-gray-600">{user?.user_metadata.email}</p>
+        </div>
+        <Link href="/dashboard">
+          <Button
+            variant="ghost"
+            className="w-full flex justify-between items-center"
+          >
+            Dashboard
+          </Button>
+        </Link>
+
+        <Button
+          variant="ghost"
+          className="w-full flex justify-between items-center"
+        >
+          Log out
+        </Button>
+      </PopoverContent>
     </Popover>
   );
 };
