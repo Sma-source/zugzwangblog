f3b45a966f580280a64dbd101264cad909f63b27
Sma-source <rabhi.smain@gmail.com>
2024-02-19 22:12:06 +0100

    modify dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="space-y-5">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">Blogs</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
