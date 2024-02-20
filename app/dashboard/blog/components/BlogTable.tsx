c9c99f4a53ec5145c5f73e336bd0e3e25f8ff9b1
Sma-source <rabhi.smain@gmail.com>
2024-02-21 00:08:00 +0100

    install & use switch ui

--- app/dashboard/blog/components/BlogTable.tsx
+++ app/dashboard/blog/components/BlogTable.tsx
@@ -1,15 +1,21 @@
 import { Button } from "@/components/ui/button";
 import { EyeOpenIcon, Pencil1Icon } from "@radix-ui/react-icons";
+import { Switch } from "@/components/ui/switch";
 import React from "react";
 
 const BlogTable = () => {
   return (
     <div className="rounded-md dark:bg-graident-dark border-[0.5px] overflow-y-scroll">
       <div className="w-[800px] md:w-full">
-        <div className="grid grid-cols-5 border-b p-5 dark:text-gray-500">
+        <div className="grid grid-cols-5 border-b p-5 dark:text-gray-500 text-zinc-500">
           <h1 className=" col-span-2">Title</h1>
           <h1>Premium</h1>
           <h1>Publish</h1>
+        </div>
+        <div className="grid grid-cols-5 p-5">
+          <h1 className="col-span-2">BlogTitle</h1>
+          <Switch checked={false} />
+          <Switch checked={true} />
           <Actions />
         </div>
       </div>
