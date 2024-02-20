afd329b53f2ea1518068dcd778e8006142cd6218
Sma-source <rabhi.smain@gmail.com>
2024-02-20 18:01:39 +0100

    modify ui

--- app/dashboard/components/NavLinks.tsx
+++ app/dashboard/components/NavLinks.tsx
@@ -1,13 +1,20 @@
+import { PersonIcon, ReaderIcon } from "@radix-ui/react-icons";
 import React from "react";
 
 const NavLinks = () => {
   const links = [
     {
       href: "/dashboard",
+      Icon: ReaderIcon,
       text: "dashboard",
     },
+    {
+      href: "/dashboard/user",
+      Icon: PersonIcon,
+      text: "users",
+    },
   ];
-  return <div>NavLinks</div>;
+  return <div className="flex items-center gap-5 border-b pb-2">NavLinks</div>;
 };
 
 export default NavLinks;
