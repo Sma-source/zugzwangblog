66fff1f4654c5720c052a22646c88040c3798610
Sma-source <rabhi.smain@gmail.com>
2024-02-20 18:44:35 +0100

    modify ui navlinks

--- app/dashboard/components/NavLinks.tsx
+++ app/dashboard/components/NavLinks.tsx
@@ -1,7 +1,12 @@
+"use client";
+import { cn } from "@/lib/utils";
 import { PersonIcon, ReaderIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const NavLinks = () => {
+  const pathname = usePathname();
   const links = [
     {
       href: "/dashboard",
@@ -14,7 +19,24 @@ const NavLinks = () => {
       text: "users",
     },
   ];
-  return <div className="flex items-center gap-5 border-b pb-2">NavLinks</div>;
+  return (
+    <div className="flex items-center gap-5 border-b pb-2">
+      {links.map(({ href, Icon, text }, index) => {
+        return (
+          <Link
+            href={href}
+            key={index}
+            className={cn(
+              "text-sm text-gray-400 flex  items-center gap-1 hover:underline transition-all",
+              { "text-violet-500 underline": pathname === href }
+            )}
+          >
+            <Icon /> / {text}
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavLinks;
