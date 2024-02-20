a942f5c79d92b8017f4d9a5c565dbd1ac18c5f9a
Sma-source <rabhi.smain@gmail.com>
2024-02-20 20:45:48 +0100

    modify ui add create blog button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 import React from "react";
 
 const Dashboard = () => {
@@ -5,6 +8,11 @@ const Dashboard = () => {
     <div className="space-y-5">
       <div className="flex items-center justify-between">
         <h1 className="text-3xl font-bold">Blogs</h1>
+        <Link href="/dashboard/blog/create">
+          <Button variant="outline" className="flex items-center">
+            Create <PlusIcon />
+          </Button>
+        </Link>
       </div>
     </div>
   );
