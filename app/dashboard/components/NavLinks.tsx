1a8b8e657f77003214094f23c79459e689ae7315
Sma-source <rabhi.smain@gmail.com>
2024-02-20 17:59:05 +0100

    create new component

---
+++ app/dashboard/components/NavLinks.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavLinks = () => {
+  const links = [
+    {
+      href: "/dashboard",
+      text: "dashboard",
+    },
+  ];
+  return <div>NavLinks</div>;
+};
+
+export default NavLinks;
