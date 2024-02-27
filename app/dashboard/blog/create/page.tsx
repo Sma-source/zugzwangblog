12e25fc5bf939e7f2b16a6833fe499eafb0e2bcb
Sma-source <rabhi.smain@gmail.com>
2024-02-27 18:44:03 +0100

    add validation for imag_url

--- app/dashboard/blog/create/page.tsx
+++ app/dashboard/blog/create/page.tsx
@@ -28,19 +28,35 @@ import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
 import MarkdownPreview from "@/components/markdown/MarkdownPreview";
 
-const FormSchema = z.object({
-  title: z.string().min(10, {
-    message: "title is too short",
-  }),
-  content: z.string().min(50, {
-    message: "Content is too short",
-  }),
-  image_url: z.string().url({
-    message: "Invalid url",
-  }),
-  is_premium: z.boolean(),
-  is_published: z.boolean(),
-});
+const FormSchema = z
+  .object({
+    title: z.string().min(10, {
+      message: "title is too short",
+    }),
+    content: z.string().min(50, {
+      message: "Content is too short",
+    }),
+    image_url: z.string().url({
+      message: "Invalid url",
+    }),
+    is_premium: z.boolean(),
+    is_published: z.boolean(),
+  })
+  .refine(
+    (data) => {
+      const image_url = data.image_url;
+      try {
+        const url = new URL(image_url);
+        return url.hostname === "images.unsplash.com";
+      } catch {
+        return false;
+      }
+    },
+    {
+      message: "Currently we are supporting only the image from unsplash",
+      path: ["image_url"],
+    }
+  );
 
 const CreateForm = () => {
   const [isPreview, setPreview] = useState(false);
@@ -78,7 +94,11 @@ const CreateForm = () => {
               role="button"
               tabIndex={0}
               className="flex gap-2 items-center border px-3 py-2 rounded-md hover:border-zinc-500 transition-all bg-zinc-100 text-sm"
-              onClick={() => setPreview(!isPreview)}
+              onClick={() =>
+                setPreview(
+                  !isPreview && !form.getFieldState("image_url").invalid
+                )
+              }
             >
               {isPreview ? (
                 <>
