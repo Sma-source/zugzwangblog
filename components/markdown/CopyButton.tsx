9ed2b9fb897fbdf6b353f7c8583916dd82e6e1b9
Sma-source <rabhi.smain@gmail.com>
2024-02-27 14:56:26 +0100

    create function copyclipboard

--- components/markdown/CopyButton.tsx
+++ components/markdown/CopyButton.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { CopyIcon } from "lucide-react";
+import React, { useState } from "react";
 
-const CopyButton = () => {
-  return <div>CopyButton</div>;
+const CopyButton = ({ id }: { id: string }) => {
+  const [OnCopy, setOnCopy] = useState(false);
+  const handleCopy = async () => {
+    let text = document.getElementById(id)!.textContent;
+    try {
+      await navigator.clipboard.writeText(text!);
+      setOnCopy(true);
+    } catch (err) {
+      console.error("Failed to copy", err);
+    }
+  };
+  return (
+    <div>
+      <CopyIcon />
+    </div>
+  );
 };
 
 export default CopyButton;
