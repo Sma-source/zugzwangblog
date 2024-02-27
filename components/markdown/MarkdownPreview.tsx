9c723b3e5c583dc081520110c4b91c98d37e67d4
Sma-source <rabhi.smain@gmail.com>
2024-02-27 14:36:06 +0100

    code custom style

--- components/markdown/MarkdownPreview.tsx
+++ components/markdown/MarkdownPreview.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import React from "react";
 import Markdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
+import { CodeIcon } from "@radix-ui/react-icons";
 import "highlight.js/styles/atom-one-dark.min.css";
 
 const MarkdownPreview = ({
@@ -33,7 +34,40 @@ const MarkdownPreview = ({
         p: ({ node, ...props }) => {
           return <p {...props} className="text-left  dark:text-gray-400" />;
         },
-        
+        code: ({ node, className, children, ...props }) => {
+          const match = /language-(\w+)/.exec(className || "");
+
+          if (match?.length) {
+            let Icon = CodeIcon;
+
+            return (
+              <div className=" bg-black text-gray-300 border-[0.5px] rounded-md border-zinc-500">
+                <div className="flex items-center justify-between px-5 py-2 border-b-[0.5px] border-zinc-500">
+                  <div className="flex items-center gap-2">
+                    <Icon />
+                    <p className="text-sm text-gray-400">
+                      {/* @ts-ignore  */}
+                      {node?.data?.meta}
+                    </p>
+                  </div>
+                </div>
+                <div className="overflow-x-auto w-full">
+                  <div className="p-5">{children}</div>
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              // TODO: convert to code block
+              <code
+                className="text-lg break-words bg-zinc-700 px-1 rounded-sm"
+                {...props}
+              >
+                {children}
+              </code>
+            );
+          }
+        },
       }}
     >
       {content}
