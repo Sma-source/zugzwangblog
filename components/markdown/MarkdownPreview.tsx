a913cbbe3d841abafd72ec99f0afc860abf20b6d
Sma-source <rabhi.smain@gmail.com>
2024-02-27 13:58:47 +0100

    add markdown component and update style components

--- components/markdown/MarkdownPreview.tsx
+++ components/markdown/MarkdownPreview.tsx
@@ -1,4 +1,8 @@
+import { cn } from "@/lib/utils";
 import React from "react";
+import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/atom-one-dark.min.css";
 
 const MarkdownPreview = ({
   content,
@@ -7,7 +11,34 @@ const MarkdownPreview = ({
   content: string;
   className?: string;
 }) => {
-  return <div>{content} </div>;
+  return (
+    <Markdown
+      className={cn("space-y-6", className)}
+      rehypePlugins={[rehypeHighlight]}
+      components={{
+        h1: ({ node, ...props }) => {
+          return <h1 {...props} className="text-3xl font-extrabold"></h1>;
+        },
+        h2: ({ node, ...props }) => {
+          return <h2 {...props} className="text-2xl font-bold mt-10 mb-10" />;
+        },
+        h3: ({ node, ...props }) => {
+          return (
+            <h3 {...props} className="text-xl font-semibold mt-10 mb-10" />
+          );
+        },
+        a: ({ node, ...props }) => {
+          return <a {...props} className="font-semibold underline" />;
+        },
+        p: ({ node, ...props }) => {
+          return <p {...props} className="text-left  dark:text-gray-400" />;
+        },
+        
+      }}
+    >
+      {content}
+    </Markdown>
+  );
 };
 
 export default MarkdownPreview;
