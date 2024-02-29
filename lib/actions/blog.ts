5380684de5abc3bca3974c2b7274be1abfaa58d7
Sma-source <rabhi.smain@gmail.com>
2024-02-29 18:37:45 +0100

    delete server connection function a use one as import to avoid cookie problem

--- lib/actions/blog.ts
+++ lib/actions/blog.ts
@@ -1,47 +1,46 @@
 "use server";
 
 import { BlogFormSchemaType } from "@/app/dashboard/schema";
-import { CookieOptions, createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
-import { Database } from "../types/supabase";
-import { revalidatePath } from "next/cache";
 
-const cookieStore = cookies();
+import { revalidatePath } from "next/cache";
+import { createSupabaseServerClient } from "../supabase";
+
+// const cookieStore = cookies();
 const DASHBOARD = "/dashboard/blog";
 
-const supabase = createServerClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    cookies: {
-      get(name: string) {
-        return cookieStore.get(name)?.value;
-      },
-      set(name: string, value: string, options: CookieOptions) {
-        try {
-          cookieStore.set({ name, value, ...options });
-        } catch (error) {
-          // The `set` method was called from a Server Component.
-          // This can be ignored if you have middleware refreshing
-          // user sessions.
-        }
-      },
-      remove(name: string, options: CookieOptions) {
-        try {
-          cookieStore.set({ name, value: "", ...options });
-        } catch (error) {
-          // The `delete` method was called from a Server Component.
-          // This can be ignored if you have middleware refreshing
-          // user sessions.
-        }
-      },
-    },
-  }
-);
+// const supabase = createServerClient<Database>(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+//   {
+//     cookies: {
+//       get(name: string) {
+//         return cookieStore.get(name)?.value;
+//       },
+//       set(name: string, value: string, options: CookieOptions) {
+//         try {
+//           cookieStore.set({ name, value, ...options });
+//         } catch (error) {
+//           // The `set` method was called from a Server Component.
+//           // This can be ignored if you have middleware refreshing
+//           // user sessions.
+//         }
+//       },
+//       remove(name: string, options: CookieOptions) {
+//         try {
+//           cookieStore.set({ name, value: "", ...options });
+//         } catch (error) {
+//           // The `delete` method was called from a Server Component.
+//           // This can be ignored if you have middleware refreshing
+//           // user sessions.
+//         }
+//       },
+//     },
+//   }
+// );
 
 export const createBlog = async (data: BlogFormSchemaType) => {
   const { ["content"]: excludedKey, ...blog } = data;
-
+  const supabase = await createSupabaseServerClient();
   const resultBlog = await supabase
     .from("blog")
     .insert(blog)
@@ -61,6 +60,7 @@ export const createBlog = async (data: BlogFormSchemaType) => {
 };
 
 export const readBlog = async () => {
+  const supabase = await createSupabaseServerClient();
   return supabase
     .from("blog")
     .select("*")
@@ -69,13 +69,24 @@ export const readBlog = async () => {
 };
 
 export const readBlogAdmin = async () => {
+  const supabase = await createSupabaseServerClient();
   return supabase
     .from("blog")
     .select("*")
     .order("created_at", { ascending: false });
 };
 
+export const readBlogDetailById = async (blogId: string) => {
+  const supabase = await createSupabaseServerClient();
+  return await supabase
+    .from("blog")
+    .select("*,blog_content(*)")
+    .eq("id", blogId)
+    .single();
+};
+
 export const deleteBlogById = async (blogId: string) => {
+  const supabase = await createSupabaseServerClient();
   const result = await supabase.from("blog").delete().eq("id", blogId);
   revalidatePath(DASHBOARD);
   return JSON.stringify(result);
@@ -85,6 +96,7 @@ export const updateBlogById = async (
   blogId: string,
   data: BlogFormSchemaType
 ) => {
+  const supabase = await createSupabaseServerClient();
   const result = await supabase.from("blog").update(data).eq("id", blogId);
   revalidatePath(DASHBOARD);
   return JSON.stringify(result);
