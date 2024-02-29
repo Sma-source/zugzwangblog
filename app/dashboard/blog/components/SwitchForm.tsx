29c32927de9bfd1031433048c68d0d9555b9cd1c
Sma-source <rabhi.smain@gmail.com>
2024-02-29 15:58:12 +0100

    add onsubmit function

--- app/dashboard/blog/components/SwitchForm.tsx
+++ app/dashboard/blog/components/SwitchForm.tsx
@@ -1,17 +1,29 @@
 "use client";
 import { Switch } from "@/components/ui/switch";
-import React from "react";
+import { toast } from "@/components/ui/use-toast";
+import React, { ChangeEvent } from "react";
 
 const SwitchForm = ({
   checked,
   onToggle,
+  name,
 }: {
   checked: boolean;
   onToggle: () => Promise<string>;
+  name: string;
 }) => {
+  const onSubmit = async (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { error } = JSON.parse(await onToggle());
+    if (!error) {
+      toast({
+        title: `Successfully update ${name} 🎉`,
+      });
+    }
+  };
   return (
-    <form>
-      <Switch checked={checked} type="submit" />
+    <form onSubmit={onSubmit}>
+      <Switch checked={checked} type="submit" className="bg-violet-500" />
     </form>
   );
 };
