03e10e94e5f29e9484a758aca4a37ce2bec102a6
Sma-source <rabhi.smain@gmail.com>
2024-02-29 18:42:41 +0100

    get data with id and pass them to editform component

--- app/dashboard/blog/edit/[id]/page.tsx
+++ app/dashboard/blog/edit/[id]/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import EditForm from "./components/EditForm";
+import { readBlogDetailById } from "@/lib/actions/blog";
+import { IBlogDetail } from "@/lib/types";
 
-const Edit = () => {
-  return <div>Edit</div>;
+const Edit = async ({ params }: { params: { id: string } }) => {
+  const { data: blog } = await readBlogDetailById(params.id);
+  return <EditForm blog={blog as IBlogDetail} />;
 };
 
 export default Edit;
