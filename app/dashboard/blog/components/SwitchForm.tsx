51574c44da36bb9cd106a8851313f1584eacfb23
Sma-source <rabhi.smain@gmail.com>
2024-02-29 15:43:57 +0100

    add switch component and types checked

--- app/dashboard/blog/components/SwitchForm.tsx
+++ app/dashboard/blog/components/SwitchForm.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { Switch } from "@/components/ui/switch";
 import React from "react";
 
-const SwitchForm = () => {
-  return <div>SwitchForm</div>;
+const SwitchForm = ({ checked }: { checked: boolean }) => {
+  return (
+    <form>
+      <Switch checked={checked} type="submit" />
+    </form>
+  );
 };
 
 export default SwitchForm;
