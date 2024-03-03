649dc1ad5dfa0a266c2d5a83d95714de4de3a5b9
Sma-source <rabhi.smain@gmail.com>
2024-03-03 18:19:12 +0100

    add logo

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,14 +4,23 @@ import React from "react";
 import LoginForm from "./LoginForm";
 import { useUser } from "@/lib/store/user";
 import Profile from "./Profile";
+import { cn } from "@/lib/utils";
+import { FaChessPawn } from "react-icons/fa";
 
 const Navbar = () => {
   const user = useUser((state) => state.user);
   return (
     <nav className="w-full justify-between items-center flex p-5 xl:p-0">
-      <Link className="font-bold text-2xl" href="/">
-        Zugzwang Blog
-      </Link>
+      <div className="group">
+        <Link className="font-bold text-2xl gap-0 inline-flex" href="/">
+          Zug
+          <FaChessPawn />
+          Zwang
+        </Link>
+        <div
+          className={cn("h-1 w-0 group-hover:w-full  transition-all bg-black")}
+        ></div>
+      </div>
       {user ? <Profile /> : <LoginForm />}
     </nav>
   );
