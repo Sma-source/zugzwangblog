5de2eceb637c8b8ffc469531770ecf90f6db0346
Sma-source <rabhi.smain@gmail.com>
2024-03-03 20:25:28 +0100

    create loading component to diplay load while data

---
+++ app/blog/[id]/components/Loading.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="animate-pulse space-y-5 p-10">
+      <h1 className=" h-10 text-3xl font-bold bg-zinc-300 dark:bg-graident-dark w-full rounded-md" />
+      <h1 className=" h-5 text-3xl font-bold bg-zinc-300 dark:bg-graident-dark w-full rounded-md" />
+      <h1 className=" h-5 text-3xl font-bold bg-zinc-300 dark:bg-graident-dark w-full rounded-md" />
+      <h1 className=" h-10 text-3xl font-bold bg-zinc-300 dark:bg-graident-dark w-full rounded-md" />
+      <h1 className=" h-5 text-3xl font-bold bg-zinc-300 dark:bg-graident-dark w-full rounded-md" />
+    </div>
+  );
+};
+
+export default Loading;
