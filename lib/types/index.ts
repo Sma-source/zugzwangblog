f98cc7d6626b31692d93c50b33d97a76a65c17c0
Sma-source <rabhi.smain@gmail.com>
2024-03-09 18:31:46 +0100

    addnew type for user

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -21,3 +21,15 @@ export type IBlogDetail = {
     created_at: string;
   };
 } | null;
+
+export type IUser = {
+  created_at: string;
+  display_name: string;
+  email: string;
+  id: string;
+  image_url: string;
+  role: string;
+  stripe_customer_id: string | null;
+  stripe_subscription_id: string | null;
+  subscription_status: boolean;
+} | null;
