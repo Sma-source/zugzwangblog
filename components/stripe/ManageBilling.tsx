a51367379baed98babaf4b16833b52fe8d2818b6
Sma-source <rabhi.smain@gmail.com>
2024-03-10 14:07:13 +0100

    create submit function to use managebilling

--- components/stripe/ManageBilling.tsx
+++ components/stripe/ManageBilling.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+"use client";
+import React, { ChangeEvent, useTransition } from "react";
 import { Button } from "../ui/button";
 import { BackpackIcon } from "@radix-ui/react-icons";
+import { useUser } from "@/lib/store/user";
+import { manageBilling } from "@/lib/actions/stripe";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { cn } from "@/lib/utils";
 
 const ManageBilling = () => {
+  const user = useUser((state) => state.user);
+  const [isPending, startTransition] = useTransition();
+  const submit = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    startTransition(async () => {
+      const data = JSON.parse(await manageBilling(user?.stripe_customer_id!));
+      window.location.href = data.url;
+    });
+  };
   return (
-    <Button
-      variant="ghost"
-      className="w-full flex justify-between items-center"
-    >
-      <span className="flex items-center gap-2">Billing</span>
-      <BackpackIcon />
-    </Button>
+    <form onSubmit={submit}>
+      <Button
+        variant="ghost"
+        className="w-full flex justify-between items-center"
+      >
+        <span className="flex items-center gap-2">
+          <AiOutlineLoading3Quarters
+            className={cn(" animate-spin", { hidden: !isPending })}
+          />
+          Billing
+        </span>
+        <BackpackIcon />
+      </Button>
+    </form>
   );
 };
 
