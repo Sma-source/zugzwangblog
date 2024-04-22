446784fac8678a7e4c1a22f84e4c038eea21757a
Sma-source <rabhi.smain@gmail.com>
2024-04-22 22:28:17 +0200

    change skeleton bg color

--- app/(home)/(default)/loading.tsx
+++ app/(home)/(default)/loading.tsx
@@ -6,13 +6,13 @@ const loading = () => {
       {[1, 2, 3, 4, 5]?.map((_, index) => {
         return (
           <div
-            className="w-full  border-0 rounded-md bg-[#F2DFCE] dark:bg-graident-dark p-5 hover:ring-2 ring-[#F2DFCE] transition-all cursor-pointer space-y-5 first:lg:col-span-2 first:md:col-span-3"
+            className="w-full animate-pulse  border-0 rounded-md bg-gray-300 dark:bg-graident-dark p-5 hover:ring-2 ring-gray-300 transition-all cursor-pointer space-y-5 first:lg:col-span-2 first:md:col-span-3"
             key={index}
           >
             <div className="w-full h-72 sm:w-full  md:h-64 xl:h-96  relative"></div>
             <div className="space-y-2">
-              <p className="text-sm dark:text-gray-400 h-5 bg-gray-500"></p>
-              <p className="text-sm dark:text-gray-400 h-10 bg-gray-500"></p>
+              <p className="text-sm dark:text-gray-200 h-5 bg-gray-300"></p>
+              <p className="text-sm dark:text-gray-200 h-10 bg-gray-300"></p>
             </div>
           </div>
         );
