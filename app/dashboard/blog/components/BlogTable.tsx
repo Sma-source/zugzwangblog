30f963a9aca66ef5fcbd6a2d30fcdebf9e268c29
Sma-source <rabhi.smain@gmail.com>
2024-04-29 20:57:42 +0200

    update table ui/ux

--- app/dashboard/blog/components/BlogTable.tsx
+++ app/dashboard/blog/components/BlogTable.tsx
@@ -6,63 +6,140 @@ import DeleteAlert from "./DeleteAlert";
 import SwitchForm from "./SwitchForm";
 import { BlogFormSchemaType } from "../../schema";
 import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import { AlertDialog, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 
 const BlogTable = async () => {
   const { data: blogs } = await readBlogAdmin();
   return (
-    <div className="rounded-md dark:bg-graident-dark bg-white border-[0.5px]  overflow-y-scroll">
-      <div className="sm:w-[900px] md:w-full">
-        <div className="grid grid-cols-5 border-b p-5 dark:text-gray-500 text-zinc-700 text-sm md:text-base">
-          <h2 className=" col-span-2">Title</h2>
-          <h1>Premium</h1>
-          <h1>Publish</h1>
+    <Card className="overflow-y-scroll">
+      {/* <CardHeader>
+        <CardTitle>Blogs</CardTitle>
+        <CardDescription></CardDescription>
+      </CardHeader> */}
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Title</TableHead>
+              <TableHead>Premium</TableHead>
+              <TableHead className="hidden md:table-cell">Publish</TableHead>
+
+              <TableHead className="hidden md:table-cell">Created at</TableHead>
+              <TableHead>
+                <span className="sr-only">Actions</span>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {blogs?.map((blog, index) => {
+              const updatePremium = updateBlogById.bind(null, blog.id, {
+                is_premium: !blog.is_premium,
+              } as BlogFormSchemaType);
+              const updatePublish = updateBlogById.bind(null, blog.id, {
+                is_published: !blog.is_published,
+              } as BlogFormSchemaType);
+              return (
+                <TableRow key={index}>
+                  <TableCell className="font-medium">{blog.title}</TableCell>
+                  <TableCell>
+                    <SwitchForm
+                      checked={blog.is_premium}
+                      name="premium"
+                      onToggle={updatePremium}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <SwitchForm
+                      checked={blog.is_published}
+                      name="published"
+                      onToggle={updatePublish}
+                    />
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    {blog.created_at}
+                  </TableCell>
+                  <Actions blogId={blog.id} />
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </CardContent>
+      <CardFooter>
+        <div className="text-xs text-muted-foreground">
+          Showing <strong>1-10</strong> of <strong>32</strong> products
         </div>
-        {blogs?.map((blog, index) => {
-          const updatePremium = updateBlogById.bind(null, blog.id, {
-            is_premium: !blog.is_premium,
-          } as BlogFormSchemaType);
-          const updatePublish = updateBlogById.bind(null, blog.id, {
-            is_published: !blog.is_published,
-          } as BlogFormSchemaType);
-          return (
-            <div className="grid grid-cols-5 p-5" key={index}>
-              <h1 className="col-span-2">{blog.title} </h1>
-              <SwitchForm
-                checked={blog.is_premium}
-                name="premium"
-                onToggle={updatePremium}
-              />
-              <SwitchForm
-                checked={blog.is_published}
-                name="published"
-                onToggle={updatePublish}
-              />
-              <Actions blogId={blog.id} />
-            </div>
-          );
-        })}
-      </div>
-    </div>
+      </CardFooter>
+    </Card>
   );
 };
 
 const Actions = ({ blogId }: { blogId: string }) => {
   return (
-    <div className="flex items-center gap-2 md:flex-wrap ">
-      <Link href={`/blog/${blogId}`}>
-        <Button className="flex gap-2  items-center" variant="outline">
-          <EyeOpenIcon />
-          View
-        </Button>
-      </Link>
-      <DeleteAlert blogId={blogId} />
-      <Link href={`/dashboard/blog/edit/${blogId}`}>
-        <Button className="flex gap-2 items-center" variant="outline">
-          <Pencil1Icon />
-          Edit
-        </Button>
-      </Link>
-    </div>
+    <TableCell>
+      <AlertDialog>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button aria-haspopup="true" size="icon" variant="ghost">
+              <MoreHorizontal className="h-4 w-4" />
+              <span className="sr-only">Toggle menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem>
+              <Link href={`/dashboard/blog/edit/${blogId}`}>Edit</Link>
+            </DropdownMenuItem>
+            <AlertDialogTrigger asChild>
+              <DropdownMenuItem>Delete</DropdownMenuItem>
+            </AlertDialogTrigger>
+            <DropdownMenuItem>
+              <Link href={`/blog/${blogId}`}>View</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <DeleteAlert blogId={blogId} />
+      </AlertDialog>
+    </TableCell>
+    // <div className="flex items-center gap-2 md:flex-wrap ">
+    //   <Link href={`/blog/${blogId}`}>
+    //     <Button className="flex gap-2  items-center" variant="outline">
+    //       <EyeOpenIcon />
+    //       View
+    //     </Button>
+    //   </Link>
+    //   <DeleteAlert blogId={blogId} />
+    //   <Link href={`/dashboard/blog/edit/${blogId}`}>
+    //     <Button className="flex gap-2 items-center" variant="outline">
+    //       <Pencil1Icon />
+    //       Edit
+    //     </Button>
+    //   </Link>
+    // </div>
   );
 };
 
