f8a47939571c51f5810ad3da26bbf49d815e9063
Sma-source <rabhi.smain@gmail.com>
2024-04-30 13:32:30 +0200

    add analytic ui top view page

--- app/dashboard/analytics/page.tsx
+++ app/dashboard/analytics/page.tsx
@@ -1,6 +1,23 @@
-import { Activity, CreditCard, Users, Eye } from "lucide-react";
+import { Activity, CreditCard, Users, Eye, ArrowUpRight } from "lucide-react";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
 
 const Analytics = () => {
   return (
@@ -57,6 +74,57 @@ const Analytics = () => {
               <p className="text-xs text-muted-foreground">
                 +201 since last hour
               </p>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+          <Card className="xl:col-span-2">
+            <CardHeader className="flex flex-row items-center">
+              <div className="grid gap-2">
+                <CardTitle>Top Content</CardTitle>
+                <CardDescription>
+                  Recent transactions from your store.
+                </CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>URL</TableHead>
+                    <TableHead className="text-right">Views</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>
+                      <div className="font-medium">/</div>
+                    </TableCell>
+                    <TableCell className="text-right">2.5K</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>
+                      <div className="font-medium"> /blog/tesst-du-1403</div>
+                    </TableCell>
+                    <TableCell className="text-right">1.5K</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>
+                      <div className="font-medium">
+                        {" "}
+                        blog/080bdc90-ef61-4fec-85ca
+                      </div>
+                    </TableCell>
+                    <TableCell className="text-right">1.1K</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>
+                      <div className="font-medium">/about/</div>
+                    </TableCell>
+                    <TableCell className="text-right">1K</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
             </CardContent>
           </Card>
         </div>
