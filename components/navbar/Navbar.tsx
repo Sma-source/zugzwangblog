5bc75801d2c8055189c30809c434c83d131db4a2
Sma-source <rabhi.smain@gmail.com>
2024-06-30 13:59:53 +0200

    create mobile nav

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -6,23 +6,119 @@ import { useUser } from "@/lib/store/user";
 import Profile from "./Profile";
 import { cn } from "@/lib/utils";
 import { GiTechnoHeart } from "react-icons/gi";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
 
 const Navbar = () => {
   const user = useUser((state) => state.user);
   return (
-    <nav className="w-full justify-between items-center flex p-5 xl:p-0">
-      <div className="group">
-        <Link className="font-bold text-2xl gap-0 inline-flex" href="/">
-          Techno
-          <GiTechnoHeart />
-          Gatsby
-        </Link>
-        <div
-          className={cn("h-1 w-0 group-hover:w-full  transition-all bg-black")}
-        ></div>
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center">
+        <div className="mr-4 hidden md:flex">
+          <Link className="mr-6 flex items-center space-x-2" href="/">
+            <GiTechnoHeart />
+            <span className="hidden font-bold sm:inline-block">
+              Techno Gatsby
+            </span>
+          </Link>
+          <nav className="flex items-center gap-4 text-sm lg:gap-6">
+            <Link
+              href="/about"
+              className="transition-colors hover:text-foreground/80 text-foreground/60"
+            >
+              A Propos
+            </Link>
+            <Link
+              href="/about"
+              className="transition-colors hover:text-foreground/80 text-foreground/60"
+            >
+              Presse
+            </Link>
+            <Link
+              href="/about"
+              className="transition-colors hover:text-foreground/80 text-foreground/60"
+            >
+              Contact
+            </Link>
+          </nav>
+          <div
+            className={cn(
+              "h-1 w-0 group-hover:w-full  transition-all bg-black"
+            )}
+          ></div>
+        </div>
+        <Sheet>
+          <SheetTrigger asChild>
+            <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 py-2 mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden">
+              <svg
+                strokeWidth="1.5"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+              >
+                <path
+                  d="M3 5H11"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+                <path
+                  d="M3 12H16"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+                <path
+                  d="M3 19H21"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+              </svg>
+            </button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetTitle></SheetTitle>
+            <SheetDescription></SheetDescription>
+            <Link href={"/"} className="flex items-center space-x-2">
+              <GiTechnoHeart />
+              <span className="font-bold text-sm">Techno Gatsby</span>
+            </Link>
+            <div className="flex flex-col space-y-3  mt-4">
+              <Link href={"/about"}>About</Link>
+              <Link href={"/about"}>Presse</Link>
+              <Link href={"/about"}>Contact</Link>
+            </div>
+          </SheetContent>
+        </Sheet>
+
+        <div className="flex flex-1 items-center  space-x-2 justify-end">
+          <nav className="flex items-center">
+            {user ? <Profile /> : <LoginForm />}
+          </nav>
+        </div>
       </div>
-      {user ? <Profile /> : <LoginForm />}
-    </nav>
+    </header>
   );
 };
 
