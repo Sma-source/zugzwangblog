a8a55e3d4056d27ba4723222fb43c9af9513b0c4
Sma-source <rabhi.smain@gmail.com>
2024-07-03 17:37:04 +0200

    change icon color

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -31,8 +31,8 @@ const Navbar = () => {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 hidden md:flex">
-          <Link className="mr-6 flex items-center space-x-2" href="/">
-            <GiTechnoHeart />
+          <Link className="mr-6 flex items-center space-x-2 " href="/">
+            <GiTechnoHeart className="text-[#ff2975]" />
             <span className="hidden font-bold sm:inline-block">
               Techno Gatsby
             </span>
