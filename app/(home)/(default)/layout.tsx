4e8b5aa74df044913637040c54938746b9fcaa13
Sma-source <rabhi.smain@gmail.com>
2024-07-03 17:40:47 +0200

    import Hero components

--- app/(home)/(default)/layout.tsx
+++ app/(home)/(default)/layout.tsx
@@ -1,9 +1,11 @@
 import BannerNewsletter from "@/components/banner/BannerNewsletter";
+import Hero from "@/components/Hero";
 import React, { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
+      <Hero />
       {children}
       <BannerNewsletter />
     </>
